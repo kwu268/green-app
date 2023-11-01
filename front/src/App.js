128a9d5406fdb7ebec9e65758fe3c82f6be3e852
Kelvin <kelvinwu717@gmail.com>
2023-11-01 18:28:54 -0400

    - added nav bar component

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,11 @@
+import React from 'react' 
 import './Global.css'
+import Navbar from './components/Navbar.js'
 
 function App() {
   return (
-    <div className="underline">
-      
+    <div >
+      <Navbar/>
     </div>
   );
 }
